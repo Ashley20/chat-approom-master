bbcfffe57198ce3caf73d8d3b0308e4c0abda7f7
Eylul <unal.eylul@student.atilim.edu.tr>
2018-04-14 15:12:17 +0300

    contacts eklendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomePage } from '../pages/home/home';
 import { SigninPage } from '../pages/signin/signin';
 import { RoomPage } from '../pages/room/room';
 import { AddRoomPage } from '../pages/add-room/add-room';
+import { Contacts } from '@ionic-native/contacts';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { AddRoomPage } from '../pages/add-room/add-room';
   providers: [
     StatusBar,
     SplashScreen,
+    Contacts,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
