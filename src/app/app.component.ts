7282964ab797deb1d55bbc13ff4b9799801d80fe
Mustafa Gücenmez <c1318029@student.cankaya.edu.tr>
2018-04-14 14:02:16 +0300

    firebase hatası düzeltildi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import * as firebase from 'firebase';
 import { HomePage } from '../pages/home/home';
+import {SigninPage} from "../pages/signin/signin";
 
 const config = {
   apiKey: 'AIzaSyDW-f4sDiqnDeC-kW2e1IwPtt7R5wok9NU',
@@ -18,8 +19,8 @@ const config = {
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
-
+  // rootPage:any = HomePage;
+  rootPage:any = SigninPage;
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
